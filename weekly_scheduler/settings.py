a58f8b9437deadcb4c7d2936fbb57e978abb141c
Aleem Nazer <aleem.nazer@gmail.com>
2025-06-14 22:44:18 +0500

    jwt auth updated for swagger

--- weekly_scheduler/settings.py
+++ weekly_scheduler/settings.py
@@ -170,3 +170,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header',
+            'description': 'JWT Authorization header using the Bearer scheme. Example: "Bearer your_token"',
+        }
+    },
+    'USE_SESSION_AUTH': False,
+}
